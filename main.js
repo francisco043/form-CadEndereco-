2a846bb70b3720472022400bce202d03805432fa
Your Name <you@example.com>
2024-09-20 09:15:09 -0300

    alteracao de nomes no cadastro

--- main.js
+++ main.js
@@ -5,10 +5,11 @@
 //* consumo de API - https://viacep.com.br/ */
 
 const limparformulario = () =>{
-   document.getElementById('CEP').value = '';
-   document.getElementById('RUA').value = '';
-   document.getElementById('LOGRADOURO').value = '';
-   document.getElementById('UF').value = '';
+
+   document.getElementById('cep').value = '';
+   document.getElementById('rua').value = '';
+   document.getElementById('logradouro').value = '';
+   document.getElementById('uf').value = '';
 
 }
 // length e uma propriedade que indentifica a quantidade de caracteres dentro do argumento cep
@@ -16,4 +17,16 @@ const limparformulario = () =>{
 const eNumero = (numero) => /^[0-9]+$/.test(numero);
 
 const cepValido = (cep) => cep.length == 8 && eNumero(cep);
+
+//funcao para preencher formulario como campos da API
+
+const preencherformulario = (endereco)=> {
+    document.getElementById('LOGRADOURO').value = endereco.logradouro;
+    document.getElementById('LOGRADOURO').value = endereco.bairro;
+    document.getElementById('LOGRADOURO').value = endereco.localidade;
+    document.getElementById('LOGRADOURO').value = endereco.uf;
+
+
+
+}
 
